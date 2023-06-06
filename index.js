997e9fefc4dea9a65c4552a90f0d32ecf9083526
Gabriel Slayton <slaytons@slaytons.net>
2023-06-06 11:37:45 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(`IP Address: ${req.connection.remoteAddress}\n 
     Port: ${req.connection.remotePort}\n 
+    IP: ${req.socket.remoteAddress}\n
     User-Agent: ${req.headers['user-agent']} \n`);
 });
 
