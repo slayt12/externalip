2ef52898728e0ead4e538d4f8a23f166e263382a
slayt12 <slaytons@slaytons.net>
2023-08-09 12:07:26 -0500

    test

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-let header = '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"';
-
-let edgePattern = /("Microsoft Edge");v="(\d+)"/;
-let match = header.match(edgePattern);
-console.log(match);
-if (match) {
-  let edgeName = match[1];
-  let edgeVersion = match[2];
-  console.log('Browser: ' + edgeName);
-  console.log('Version: ' + edgeVersion);
-}
+const res = fetch('http://ip-api.com/json');
